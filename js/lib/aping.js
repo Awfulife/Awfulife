f9de27fd1b0493fc611fe307c3a87747f73f25f3
Awfulife <awfu.life>
2016-06-12 21:35:56 -0400

    Fix date for facebook posts on certain devices

--- js/lib/aping.js
+++ js/lib/aping.js
@@ -286,6 +286,9 @@ angular.module('jtt_aping')
          * @returns {Number}
          */
         this.getTimestampFromDateString = function (_string, _multiplier, _add) {
+            if(_string.length>5 && _string.charAt(_string.length-5) == "+"){
+              _string = _string.substr(0,test.length-4) + test.substr(test.length-4, 2) + ":" + test.substr(test.length-2, 2)
+            }
             return new Date(_string); //***custom code
             if (angular.isUndefined(_multiplier) || isNaN(_multiplier)) {
                 _multiplier = 1;
